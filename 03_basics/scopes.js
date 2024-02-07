07eee1d8a78df4fc22b6369d6404271a71b51bca
Dark2711 <116933783+Dark2711@users.noreply.github.com>
2024-02-07 22:07:39 +0530

    update scope.js

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -13,8 +13,39 @@ let a = 300 // here a is declared globally that is called global scope
 
 if(true){
     let a = 10
-    console.log("INNER: ",a); // here a has its own scope that is a is in block scope
+    // console.log("INNER: ",a); // here a has its own scope that is a is in block scope
 }
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = "Prince"
+
+    function two(){
+        const email = "abc@google.com"
+        console.log(username); // here it can access username because for this block username is declared globally(outside this block)
+    }
+    // console.log(email); // here it cannot access email
+    two()
+}
+// one()
+
+if(true){
+    const username = "Spiderman"
+    if(true){
+        const email = " spider@man.com"
+        // console.log(username + email);
+    }
+    // console.log(email); //error 
+}
+// console.log(username); //error
+
+// console.log(addOne(5)) // here we can access it from any where
+function addOne(num){
+    return num + 1
+}
 
 
+// addTwo(7) // when we declare function using variable we cannot access it before initialization
+const addTwo = function(num){
+    return num +2
+}
